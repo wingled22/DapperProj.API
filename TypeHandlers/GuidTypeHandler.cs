98ce3eb70b3c3b0750fb310cb22597b1a62fdc15
pelayowindel-dynata <windel.pelayo@dynata.com>
2024-05-21 18:43:08 -0700

    added type handler for converting string to guid

---
+++ TypeHandlers/GuidTypeHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace Proj.API.TypeHandlers
+{
+    using System;
+    using Dapper;
+    using System.Data;
+
+    public class GuidTypeHandler : SqlMapper.TypeHandler<Guid>
+    {
+        public override void SetValue(IDbDataParameter parameter, Guid value)
+        {
+            parameter.Value = value.ToString();
+        }
+
+        public override Guid Parse(object value)
+        {
+            return Guid.Parse(value.ToString());
+        }
+    }
+
+}
