7481745f9692163bb74030a54909d2dc7aaddf42
pelayowindel-dynata <windel.pelayo@dynata.com>
2024-05-21 18:42:48 -0700

    added contribution

---
+++ Controllers/ContributionController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Proj.API.Models;
+using Proj.API.Repositories;
+
+namespace Proj.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ContributionController : ControllerBase
+    {
+        private ContributionRepository _contributionRepository;
+        public ContributionController(ContributionRepository contributionRepository)
+        {
+            _contributionRepository = contributionRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetContributions(int clientId)
+        {
+            try{
+                var contributions  =  await _contributionRepository.GetContributions(clientId);
+                return Ok(contributions);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Repositories/ContributionRepository.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using Proj.API.Data;
+using Proj.API.Models;
+
+namespace Proj.API.Repositories
+{
+    public class ContributionRepository
+    {
+        private DapperContext _context;
+        public ContributionRepository(DapperContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Contribution>> GetContributions(int clientId)
+        {
+            const string query = @"select * from Contributions where ClientId = @clientId";
+
+            using(var connection =  _context.CreateConnection())
+            {
+                var contributions = await connection.QueryAsync<Contribution>(query, new {clientId =clientId});
+                return contributions;
+            }
+        } 
+    }
+}
