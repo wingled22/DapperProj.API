0bb416893676042252b13422b1ba092d3836fe5b
pelayowindel-dynata <windel.pelayo@dynata.com>
2024-05-21 18:44:09 -0700

    updated some files

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
+using Dapper;
 using Proj.API.Configurations;
 using Proj.API.Data;
 using Proj.API.Repositories;
+using Proj.API.TypeHandlers;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
@@ -10,10 +12,12 @@ sqliteConnection.Validate();
 builder.Services.AddSingleton<IDatabaseProvider>(sqliteConnection);
 builder.Services.AddSingleton<DapperContext>();
 builder.Services.AddSingleton<ClientRepository>();
+builder.Services.AddSingleton<ContributionRepository>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+SqlMapper.AddTypeHandler(new GuidTypeHandler());
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
