f701097c8007d01fc656ca39206669237e655c68
pelayowindel-dynata <windel.pelayo@dynata.com>
2024-05-21 18:44:42 -0700

    added clientContributionDTO for view client contributions

---
+++ DTO/ClientContributionDTO.cs
@@ -0,0 +1,12 @@
+﻿using Proj.API.Models;
+
+namespace Proj.API.DTO
+{
+    public class ClientContributionDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public double TotalContribution { get; set; }
+        public List<Contribution> contributions { get; set; }
+    }
+}
