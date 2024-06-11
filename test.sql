f0fb1c4f4bc718ae9436d35ce1eb6f558ecc2fee
pelayowindel-dynata <windel.pelayo@dynata.com>
2024-06-11 21:21:41 +0800

    updated test.sql

--- test.sql
+++ test.sql
@@ -14,8 +14,40 @@
 --     c.Id
 -- select * from Contributions;
 
-SELECT *
-FROM Clients c 
-LEFT JOIN Contributions co ON c.Id = co.ClientId
--- GROUP BY c.Id, c.Name
-order by c.Id
+-- SELECT *
+-- FROM Clients c 
+-- LEFT JOIN Contributions co ON c.Id = co.ClientId
+-- -- GROUP BY c.Id, c.Name
+-- order by c.Id
+
+-- SELECT 
+--             c.Id,
+--             c.Name,
+--             SUM(co.Amount) AS TotalContribution,
+--             GROUP_CONCAT(co.Id) AS ContributionIds
+--         FROM 
+--             Clients c
+--         LEFT JOIN 
+--             Contributions co ON c.Id = co.ClientId
+--         -- WHERE 
+--         --     c.Id = @ClientId
+--         GROUP BY 
+--             c.Id, c.Name
+
+ SELECT 
+    c.Id,
+    c.Name,
+    COALESCE(SUM(co.Amount), 0) AS TotalContribution,
+    CASE WHEN COUNT(co.Id) > 0 THEN 
+        JSON_GROUP_ARRAY(JSON_OBJECT('Id', co.Id, 'Amount', co.Amount, 'ClientId', co.ClientId, 'DateCreated', co.DateCreated))
+    ELSE 
+        -- JSON_ARRAY()
+        null
+    END 
+    AS Contributions
+FROM 
+    Clients c
+LEFT JOIN 
+    Contributions co ON c.Id = co.ClientId
+GROUP BY 
+    c.Id, c.Name
